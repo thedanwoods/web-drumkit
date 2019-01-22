bb3837025a822dda10bd9ddf867b58477afc3216
thedanwoods <dan@danwoods.co.uk>
2019-01-22 16:31:42 +0000

    Updates

--- precache-manifest.6cff3208762e13d86adfe2ef6e354b76.js
+++ precache-manifest.6cff3208762e13d86adfe2ef6e354b76.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/web-drumkit/static/js/runtime~main.15d16d6f.js"
   },
   {
-    "revision": "38e5e699ec487251a4f9",
-    "url": "/web-drumkit/static/js/main.38e5e699.chunk.js"
+    "revision": "63a852a49f31de5083f2",
+    "url": "/web-drumkit/static/js/main.63a852a4.chunk.js"
   },
   {
     "revision": "0cd41f930773816f5aef",
     "url": "/web-drumkit/static/js/1.0cd41f93.chunk.js"
   },
   {
-    "revision": "38e5e699ec487251a4f9",
+    "revision": "63a852a49f31de5083f2",
     "url": "/web-drumkit/static/css/main.f7d57a1c.chunk.css"
   },
   {
-    "revision": "ce5e45d37177f1ca5940b829705a47c9",
+    "revision": "0bf843173f623a00b25baadc6d4a0cc4",
     "url": "/web-drumkit/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/web-drumkit/precache-manifest.0e851cdc9d0837df75f5ee1cc66df0fb.js"
+  "/web-drumkit/precache-manifest.6cff3208762e13d86adfe2ef6e354b76.js"
 );
 
 workbox.clientsClaim();
