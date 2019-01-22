69342c776e5e70e5c891ce7e45c7cf393baf6724
thedanwoods <dan@danwoods.co.uk>
2019-01-22 15:02:37 +0000

    Updates

---
+++ precache-manifest.443da2b01798e067300df113c7aebfa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1f8ec11fde72deefe92",
+    "url": "/beats/static/js/runtime~main.f1f8ec11.js"
+  },
+  {
+    "revision": "328abece24267cd4f9c5",
+    "url": "/beats/static/js/main.328abece.chunk.js"
+  },
+  {
+    "revision": "9f460dbd4385b5784cfd",
+    "url": "/beats/static/js/1.9f460dbd.chunk.js"
+  },
+  {
+    "revision": "328abece24267cd4f9c5",
+    "url": "/beats/static/css/main.f7d57a1c.chunk.css"
+  },
+  {
+    "revision": "460d4c99c8b47030f36da7a294e7bfee",
+    "url": "/beats/index.html"
+  }
+];
